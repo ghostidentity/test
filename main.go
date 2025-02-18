5860c87369d884be26edf15bc6e62b292b6ca80e
Mark <mtagab@outlook.com>
2025-02-19 04:28:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello leapcell",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.HTML(200, "index.html", gin.H{
+            "content": "<h1>Hello Leapcell</h1>",
+        })
+    })
+    r.Run() // listen and serve on 0.0.0.0:8080
 }
